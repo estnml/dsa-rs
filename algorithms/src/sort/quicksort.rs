77d6c8b8cb22f58a81aa04ca5ee11d8676621831
estnml <lightofpast@gmail.com>
2024-05-05 02:04:09 +0300

    quicksort select pivot middle element

--- algorithms/src/sort/quicksort.rs
+++ algorithms/src/sort/quicksort.rs
@@ -1,5 +1,3 @@
-
-
 // ! after partitioning procedure, pivot element is on the sorted position.
 // ! elements on the left side of the pivot are smaller.
 // ! elements on the right side of the pivot are greater.
@@ -12,33 +10,37 @@
 // TODO: 3) make implementation generic. (-)
 // TODO: 4) adapt algorithm for usage with traits
 
-pub fn quicksort_pv_first(arr: &mut [i32]) {
+pub fn quicksort_dsa_rs(arr: &mut [i32]) {
     if arr.len() >= 2 {
-        let pivot_index = 0;
-        let pivot_value = arr[pivot_index];
-        let mut store_index = pivot_index + 1;
+        let pivot_value = arr[arr.len() / 2];
 
-        for i in pivot_index + 1..arr.len() {
+        // swap first element with pivot
+        let temp = arr[0];
+        arr[0] = pivot_value;
+        arr[arr.len() / 2] = temp;
+
+        let mut swap_index = 1;
+        for i in 1..arr.len() {
             if arr[i] <= pivot_value {
-                // swap if i and store index are not pointing to same position
-                if i != store_index {
+                if i != swap_index {
+                    // swap i with swapindex
                     let temp = arr[i];
-                    arr[i] = arr[store_index];
-                    arr[store_index] = temp;
+                    arr[i] = arr[swap_index];
+                    arr[swap_index] = temp;
                 }
 
-                store_index += 1;
+                swap_index += 1;
             }
         }
 
-        let temp = arr[pivot_index];
-        arr[pivot_index] = arr[store_index - 1];
-        arr[store_index - 1] = temp;
+        // swap pivot with swapindex - 1
+        let temp = arr[swap_index - 1];
+        arr[swap_index - 1] = arr[0];
+        arr[0] = temp;
 
-        quicksort_pv_first(&mut arr[..store_index - 1]);
-        quicksort_pv_first(&mut arr[store_index..]);
+        quicksort_dsa_rs(&mut arr[..swap_index - 1]);
+        quicksort_dsa_rs(&mut arr[swap_index..]);
     } else {
         return;
     }
 }
-
